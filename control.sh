5ae0b49fbf8174314031e6fc5b2ac9c432022605
Alex McMurray <alexgmcm@gmail.com>
2012-06-10 15:27:06 +0100

    some more of script written

--- control.sh
+++ control.sh
@@ -13,15 +13,28 @@
 
 #Expected usage:
 #./control.sh eps2 mu2 eta thetai eps1 mu1 
-#./control.sh 1.0 1.0 -2.0 -1.0 3 PI/4.0 
-echo "Editing Fortran parameters to eps2= $3 , mu2= $4 , eta= $5 , thetai= $6 , eps1= $1 , mu1= $2"
+#./control.sh 1.0 1.0 "(-2.0,0.0)" -1.0 3 PI/4.0 
+echo "Editing Fortran parameters to eps1= $1 , mu1= $2 , eps2= $3 , mu2= $4 , eta= $5 , thetai= $6"
+eps1="$1"
+mu1="$2"
 eps2="$3"
 mu2="$4"
 eta="$5"
 thetai="$6"
-eps1="$1"
-mu1="$2"
 
-sed -e "s/^eps1=.*/eps1=$1/" congaussian1fixed.f90
+sed -e "s/^eps1=.*/eps1=$1/" \
+-e "s/^mu1=.*/mu1=$2/" \
+-e "s/^eps2=.*/eps2=$3/" \
+-e "s/^mu2=.*/mu2=$4/" \
+-e "s/^eta=.*/eta=$5/" \
+-e "s/^thetai=.*/thetai=$6/" \
+congaussian1fixed.f90 > congaussian1fixed.tmp
+cp congaussian1fixed.tmp congaussian1fixed.f90
+
+
+
+
+
+
 
 echo "Completed."
